28eea981170ab34dc5e703dc90d1cfdaf3925297
Andrea Jemmett <andreajemmett@gmail.com>
2017-09-20 17:56:30 +0200

    updated setup_dir script

--- work/setup_dir.sh
+++ work/setup_dir.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
 
-rm -rf $1
-mkdir $1
-pushd $1 > /dev/null
-rm -rf out in driver *.log
-mkdir out in driver
-echo A > in/seed
-popd > /dev/null
+conf_files="`ls *.conf`"
+
+for conf_file in $conf_files; do
+  folder="${conf_file%.*}"
+  echo "Setting up ${folder}"
+  rm -rf $folder
+  mkdir $folder
+  pushd $folder > /dev/null
+  rm -rf out in driver *.log
+  mkdir out in driver
+  echo A > in/seed
+  popd > /dev/null
+done
