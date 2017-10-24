749f263c5e3fea2c17dabb8e35e6ae543ae43ad0
Andrea Jemmett <andreajemmett@gmail.com>
2017-10-24 17:10:44 +0200

    minor changes in setup dir script

--- work/setup_dir.sh
+++ work/setup_dir.sh
@@ -1,19 +1,21 @@
 #!/usr/bin/env bash
 
+seed="$1"
+
 conf_files="`ls *.conf`"
 
-rm -rf ./*.log
+rm -rf ./*.log ./.input
 
 for conf_file in $conf_files; do
   arr=(${conf_file//./ })
   folder=${arr[0]}
   ftype=${arr[1]}
   echo "Setting up ${folder} (type ${ftype})"
-  rm -rf $folder
+  rm -rf $folder ".${folder}.input"
   mkdir $folder
   pushd $folder > /dev/null
   rm -rf out in driver *.log
   mkdir -p out/inject/queue out/${folder} in driver
-  echo A > in/seed
+  echo "${seed}" > in/seed
   popd > /dev/null
 done
